eb72ec67f06581b8890201d67bb186645a86f141
Yoonho Kim <Daniel@Angels-MBP.localdomain>
2020-03-02 13:23:55 -0800

    product summary

--- sprint-1/client/src/App.js
+++ sprint-1/client/src/App.js
@@ -1,7 +1,10 @@
 import React from "react";
+import Header from "./components/header";
 
-function App() {
-  return <div className="App"></div>;
+export default class App extends React.Component {
+  render() {
+    return <Header />;
+  }
 }
 
 export default App;
--- sprint-1/client/src/components/productSummary.js
+++ sprint-1/client/src/components/productSummary.js
@@ -1,5 +1,5 @@
 import React from "react";
 
-export default function header() {
+export default function productSummary() {
   return <div></div>;
 }
