c5810904b7a226526f7a3ee9cfec5cd5ba221e25
Jason <jason.lo.kc@gmail.com>
2020-03-03 15:53:09 -0800

    did 1/2 styling for inventory page

--- sprint-1/client/src/components/Inventoryfe.js
+++ sprint-1/client/src/components/Inventoryfe.js
@@ -1,5 +1,6 @@
 import React from "react";
 import removebutton from "../Assets/Icons/SVG/Icon-kebab-default.svg";
+import searchicon from "../Assets/Icons/SVG/Icon-search.svg";
 
 function Inventoryfe() {
   let inventoryElements = props.inventoryInfo.map((inventorythings, index) => {
@@ -18,10 +19,14 @@ function Inventoryfe() {
             {inventorythings.lastordered}
           </h3>
           <h2 className="inventory__location">LOCATION</h2>
-          <h3 className="inventory__location--city">{inventorythings.city}</h3>
-          <h3 className="inventory__location--country">
-            {inventorythings.country}
-          </h3>
+          <div className="inventory__location--info">
+            <h3 className="inventory__location--city">
+              {inventorythings.city}
+            </h3>
+            <h3 className="inventory__location--country">
+              {inventorythings.country}
+            </h3>
+          </div>
           <h2 className="inventory__quantity">QUANTITY</h2>
           <h3 className="inventory__quantity--value">
             {inventorythings.quantity}
@@ -39,7 +44,14 @@ function Inventoryfe() {
     <div className="inventory">
       <div className="inventory__head">
         <h2 className="inventory__head--title">Inventory</h2>
-        <input type="text" className="inventory__head--search" />
+        <input type="text" className="inventory__head--search" placeholder="" />
+      </div>
+      <div className="inventory__title">
+        <h2 className="inventory__title--item">ITEM</h2>
+        <h2 className="inventory__title--lastorder">LAST ORDERED</h2>
+        <h2 className="inventory__title--location">LOCATION</h2>
+        <h2 className="inventory__title--quantity">QUANTITY</h2>
+        <h2 className="inventory__title--status">STATUS</h2>
       </div>
       <div className="inventory__maincontent">{inventoryElements}</div>
       <button className="inventory__additem" type="button" />
