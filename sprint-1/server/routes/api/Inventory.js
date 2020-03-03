9cda9f7fa54d7377caf535c07e35ddf8d68ad871
Jason <jason.lo.kc@gmail.com>
2020-03-03 13:20:03 -0800

    finished backend inventory get and post

--- sprint-1/server/routes/api/Inventory.js
+++ sprint-1/server/routes/api/Inventory.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const inventoryFile = __dirname + "../../models/Inventory.json";
 const inventoryItems = require(inventoryFile);
+const helper = require("../../helper/helper");
 const router = express.Router();
 
 router.get("/", (req, res) => {
@@ -10,12 +11,41 @@ router.get("/", (req, res) => {
       productname: inventoryItem.productname,
       productdescription: inventoryItem.productdescription,
       lastordered: inventoryItem.lastordered,
-      location: inventoryItem.location,
+      city: inventoryItem.city,
+      country: inventoryItem.country,
       quantity: inventoryItem.quantity,
       status: inventoryItem.status
     };
   });
   res.json(inventoryList);
+});
+
+router.post("/", (req, res) => {
+  const newInventoryItem = {
+    productname: req.body.productname,
+    productdescription: req.body.productdescription,
+    lastordered: req.body.lastordered,
+    city: req.body.city,
+    country: req.body.country,
+    quantity: req.body.quantity,
+    status: req.body.status
+  };
+  if (
+    !newInventoryItem.productname ||
+    !newInventoryItem.productdescription ||
+    !newInventoryItem.lastordered ||
+    !newInventoryItem.city ||
+    !newInventoryItem.country ||
+    !newInventoryItem.quantity ||
+    !newInventoryItem.status
+  ) {
+    return res.status(400).json({
+      errorMessage: "please fill in all the blanks"
+    });
+  }
+  inventoryItems.push(newInventoryItem);
+  helper.writeJSONFile(inventoryFile, inventoryItems);
+  res.json(inventoryItems);
 });
 
 module.exports = router;
