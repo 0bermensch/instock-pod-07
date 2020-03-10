3a4aba96ba98fde9ff574c4843bc600e638bcc5f
Yoonho Kim <Daniel@Yoonhos-MacBook-Pro.local>
2020-03-09 22:58:09 -0700

    warehouse detail function done

--- sprint-1/client/src/components/Locations.js
+++ sprint-1/client/src/components/Locations.js
@@ -1,65 +1,66 @@
 import React from "react";
 import LocationsRender from "./LocationsRender";
-import Plus from "../Assets/Icons/SVG/Icon-add.svg";
+import LocationDetail from "./LocationsDetail";
+import AddBtn from "./AddBtn";
+import LocationTop from "./LocationTop";
+
 import axios from "axios";
 class Locations extends React.Component {
   constructor() {
     super();
     this.state = {
-      locationDetail: {}
+      locationDetail: true
     };
   }
 
   locationClick = id => {
     axios.get(`/api/locations/${id}`).then(res => {
-      console.log(res.data);
+      localStorage.setItem("locationDetail", JSON.stringify(res.data));
       this.setState({
-        locationDetail: res.data
+        locationDetail: false
       });
     });
   };
 
   render() {
+    const renderLocation = () => {
+      if (this.state.locationDetail) {
+        return this.props.locations.map(location => {
+          return (
+            <LocationsRender
+              id={location.id}
+              warehouse={location.warehouse}
+              address={location.address}
+              contactPerson={location.contactPerson}
+              contactPosition={location.contactPosition}
+              contactNumber={location.contactNumber}
+              contactEmail={location.contactEmail}
+              categories={location.categories}
+              locationClick={this.locationClick}
+            />
+          );
+        });
+      } else {
+        return (
+          <LocationDetail
+            location={JSON.parse(localStorage.getItem("locationDetail"))[0]}
+            inventory={this.props.inventory}
+          />
+        );
+      }
+    };
     return (
       <>
         <section className="locations">
-          <div className="locations__top">
-            <h1 className="locations__header">Locations</h1>
-            <form>
-              <input
-                className="locations__search"
-                type="text"
-                name="search"
-                placeholder="Search"
-              />
-            </form>
-          </div>
+          <LocationTop condition={this.state.locationDetail} />
           <div className="locations__labels">
             <h4 className="locations__label">warehouse</h4>
             <h4 className="locations__label">contact</h4>
             <h4 className="locations__label">contact information</h4>
             <h4 className="locations__label">categories</h4>
           </div>
-          {this.props.locations.map(location => {
-            return (
-              <>
-                <LocationsRender
-                  id={location.id}
-                  warehouse={location.warehouse}
-                  address={location.address}
-                  contactPerson={location.contactPerson}
-                  contactPosition={location.contactPosition}
-                  contactNumber={location.contactNumber}
-                  contactEmail={location.contactEmail}
-                  categories={location.categories}
-                  locationClick={this.locationClick}
-                />
-              </>
-            );
-          })}
-          <div className="locations__circle">
-            <img className="locations__plus" src={Plus} alt="add" />
-          </div>
+          {renderLocation()}
+          <AddBtn condition={this.state.locationDetail} />
         </section>
       </>
     );
--- sprint-1/client/src/components/LocationsDetail.js
+++ sprint-1/client/src/components/LocationsDetail.js
@@ -1,7 +1,31 @@
-import React, { Component } from "react";
+import React from "react";
+import WInventory from "./WInventory";
 
-export default class LocationsDetail extends Component {
-  render() {
-    return <div>Hello</div>;
-  }
+export default function LocationsDetail({ location, inventory }) {
+  return (
+    <>
+      <div key={location.id} className="locations__info">
+        <div className="locations__info-section locations__info-section--1">
+          <div className="locations__arrow-container">
+            <h4 className="locations__location">{location.warehouse}</h4>
+          </div>
+          <p className="locations__address">{location.address}</p>
+        </div>
+        <div className="locations__info-section locations__info-section--bottom">
+          <p className="locations__contact-person">{location.contactPerson}</p>
+          <p className="locations__contact-position">
+            {location.contactPosition}
+          </p>
+        </div>
+        <div className="locations__info-section locations__info-section--bottom">
+          <p className="locations__contact-number">{location.contactNumber}</p>
+          <p className="locations__contact-email">{location.contactEmail}</p>
+        </div>
+        <div className="locations__info-section locations__info-section--bottom locations__dt-arrow-container">
+          <p className="locations__categories">{location.categories}</p>
+        </div>
+      </div>
+      <WInventory inventory={inventory} />
+    </>
+  );
 }
